738911bbb567fd8e4509ec0f10cbaef1e9a0483d
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-10 22:21:03 +0000

    added all html routes and static file references

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,6 +4,15 @@ const router = require("express").Router()
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/assets/index.html"));
-})
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/assets/stats.html"));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/assets/exercise.html"));
+});
+
 
 module.exports = router;
