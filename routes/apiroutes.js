965af1ba453a873a0839d9b5ab6effc4011939d0
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-11 22:47:09 +0000

    fixed seeders file for directory

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,19 +5,22 @@ router.put("/api/workouts/:id", (req, res) => {
   const data = req.body;
   Workout.create(data)
     .then((createdWorkout) => {
-        console.log(createdWorkout)
-        res.json(createdWorkout)})
+      console.log(createdWorkout);
+      res.json(createdWorkout);
+    })
     .catch((err) => res.json(err));
 });
 
 router.post("/api/workouts", (req, res) => {
-  const data = req.body;
-  console.log(data);
+  Workout.create({})
+    .then((workoutsDB) => res.json(workoutsDB))
+    .catch((err) => res.json(err));
 });
 
 router.get("/api/workouts", (req, res) => {
-    Workout.findOne({}).then(foundWorkouts => res.json(foundWorkouts))
-    .catch(err => res.json(err))
-})
+  Workout.findOne({})
+    .then((foundWorkouts) => res.json(foundWorkouts))
+    .catch((err) => res.json(err));
+});
 
 module.exports = router;
--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,6 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let Workout = require("../models/workoutModel");
+console.log(Workout)
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -124,8 +125,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
