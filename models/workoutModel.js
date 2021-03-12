54c3dae76f9aaf12178269776b593388ac99beec
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-12 09:50:32 +0000

    api fixing

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -35,6 +35,6 @@ const WorkoutSchema = new Schema({
     }
 });
 
-const Workout = mongoose.model("User", WorkoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
