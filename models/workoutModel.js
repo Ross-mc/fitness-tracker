af6f52fa9522b1b04133767fee4a47b3719f7f6e
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-14 19:20:31 +0000

    db fixes

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -3,43 +3,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "name required"
-    },
-    type: {
-        type: String,
-        trim: true,
-        required: "type required"
-    },
-    duration: {
-        type: Number,
-        required: "duration required"
-    },
-    userCreated: {
-        type: Date,
-        default: Date.now
-    },
-    weight: {
-        type: Number
-    },
-    sets: {
-        type: Number
-    },
-    reps: {
-        type: Number
-    },
-    distance: {
-        type: Number
-    }
+    day: Date,
+    exercises: [
+        {
+            name: {
+                type: String,
+                trim: true,
+                required: "name required"
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: "type required"
+            },
+            duration: {
+                type: Number,
+                required: "duration required"
+            },
+            userCreated: {
+                type: Date,
+                default: Date.now
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
 });
 
-const WorkoutsSchema = new Schema({
-    day: Date,
-    exercises: [WorkoutSchema]
-})
 
-const Workout = mongoose.model("Workout", WorkoutsSchema);
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
